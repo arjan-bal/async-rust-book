b1a12fc069a306c82aad37a9e24194fd50669783
Arjan Bal <arjansbal@google.com>
2024-11-02 01:11:04 +0530

    Change web server to async using tokio

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,33 +1,45 @@
-use std::{
+use std::time::Duration;
+
+use tokio::{
     fs,
-    io::{Read, Write},
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
     const PORT: usize = 7878;
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT)).unwrap();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", PORT))
+        .await
+        .unwrap();
     println!("Server is running on port {}", PORT);
 
-    for stream_result in listener.incoming() {
-        let stream = stream_result.unwrap();
-        handle_connection(stream);
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(handle_connection(stream));
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
+    println!("Handling a request");
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        tokio::time::sleep(Duration::new(5, 0)).await;
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(filename).await.unwrap();
     let response = format!("{} {}", status, contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
+    println!("Completed handling request");
 }
